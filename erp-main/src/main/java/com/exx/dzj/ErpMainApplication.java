f0580b73be1bee6d887f7132125174201b674fbf
yangyun_glb <yangyun_glb@.com>
2019-01-04 11:13:13 +0800

    erp main commit by yangyun 2019年1月4日11:13:00

---
+++ erp-main/src/main/java/com/exx/dzj/ErpMainApplication.java
@@ -0,0 +1,14 @@
+package com.exx.dzj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpMainApplication.class, args);
+	}
+
+}
+
