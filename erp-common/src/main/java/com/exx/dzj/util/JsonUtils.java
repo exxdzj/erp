02c629c5407e7278eb8adabfec639497b48ad5c4
hwlsniper <1332601370@qq.com>
2019-01-09 18:07:35 +0800

    json 和 对象转换工具类

---
+++ erp-common/src/main/java/com/exx/dzj/util/JsonUtils.java
@@ -0,0 +1,81 @@
+package com.exx.dzj.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.List;
+
+/**
+ * @ClassName:  JsonUtils
+ * @Description:自定义响应结构
+ * @date:   2019年1月09日
+ */
+public class JsonUtils {
+
+    // 定义jackson对象
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    /**
+     * @Title: objectToJson
+     * @Description: 将对象转换成json字符串
+     * @author: 天刀-盛泽荣
+     * @date: 2018年6月10日 下午9:45:48
+     * @param: @param data
+     * @param: @return
+     * @return: String
+     * @throws
+     */
+    public static String objectToJson(Object data) {
+        try {
+            String string = MAPPER.writeValueAsString(data);
+            return string;
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * @Title: jsonToPojo
+     * @Description: 将json结果集转化为对象
+     * @author: 天刀-盛泽荣
+     * @date: 2018年6月10日 下午9:45:35
+     * @param: @param jsonData
+     * @param: @param beanType
+     * @param: @return
+     * @return: T
+     * @throws
+     */
+    public static <T> T jsonToPojo(String jsonData, Class<T> beanType) {
+        try {
+            T t = MAPPER.readValue(jsonData, beanType);
+            return t;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * @Title: jsonToList
+     * @Description: 将json数据转换成pojo对象list
+     * @author: 天刀-盛泽荣
+     * @date: 2018年6月10日 下午9:45:24
+     * @param: @param jsonData
+     * @param: @param beanType
+     * @param: @return
+     * @return: List<T>
+     * @throws
+     */
+    public static <T>List<T> jsonToList(String jsonData, Class<T> beanType) {
+        JavaType javaType = MAPPER.getTypeFactory().constructParametricType(List.class, beanType);
+        try {
+            List<T> list = MAPPER.readValue(jsonData, javaType);
+            return list;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
