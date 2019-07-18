7bff54d35ae9720346f431bc3f3e703ee5595427
yangyun_glb <yangyun_glb@.com>
2019-07-18 18:00:13 +0800

    杨云 2019年7月18日18:00:08

--- erp-user/src/main/java/com/exx/dzj/entity/dictionary/DictionaryInfo.java
+++ erp-user/src/main/java/com/exx/dzj/entity/dictionary/DictionaryInfo.java
@@ -11,8 +11,6 @@ import java.util.Date;
 public class DictionaryInfo implements Serializable {
     private Integer id;
 
-    private String value;
-
     private String dictCode;
 
     private String dictName;
