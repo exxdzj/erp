66e3402ecb6fb82377c252e9d5c29b746777fdcf
yangyun_glb <yangyun_glb@.com>
2019-08-17 11:24:12 +0800

    客户等级报表 杨云 2019年8月13日09:12:54

--- erp-main/src/main/java/com/exx/dzj/facade/market/SalesTicketFacade.java
+++ erp-main/src/main/java/com/exx/dzj/facade/market/SalesTicketFacade.java
@@ -473,10 +473,10 @@ public class SalesTicketFacade {
         if (b.getBuyCount() >= 20 || b.getTotalVolume().subtract(new BigDecimal(60000)).intValue() >= 0){
             b.setGradeCode("vip003");
             b.setCustGrade("钻石客户");
-        } else if(b.getBuyCount() >= 8 || b.getTotalVolume().subtract(new BigDecimal(30000)).intValue() >= 0) {
+        } else if(b.getBuyCount() >= 10 || b.getTotalVolume().subtract(new BigDecimal(36000)).intValue() >= 0) {
             b.setGradeCode("vip002");
             b.setCustGrade("铂金客户");
-        } else if(b.getBuyCount() >= 5 || b.getTotalVolume().subtract(new BigDecimal(10000)).intValue() >= 0) {
+        } else if(b.getBuyCount() >= 5 || b.getTotalVolume().subtract(new BigDecimal(12000)).intValue() >= 0) {
             b.setGradeCode("vip001");
             b.setCustGrade("黄金客户");
         } else {
